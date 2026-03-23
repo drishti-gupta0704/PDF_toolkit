ae655b4afcfdad295507dc3ee248d1082bd96e8b
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-03-23 21:58:38 +0530

    adding PDF compression route

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -6,7 +6,8 @@ const upload = require("../middleware/uploadMiddleware");
 const {
   uploadSingle,
   uploadMultiple, 
-  mergeFiles
+  mergeFiles,
+  compressFile, 
 } = require("../controllers/fileController");
 
 // single file
@@ -14,7 +15,7 @@ router.post("/upload", upload.single("file"), uploadSingle);
 
 // multiple files
 router.post("/upload-multiple", upload.array("files", 5), uploadMultiple);
-
+router.post("/compress", upload.single("file"), compressFile);
 router.post("/merge", upload.array("files", 10), mergeFiles);
 
 module.exports = router;
